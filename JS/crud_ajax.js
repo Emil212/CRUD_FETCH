b649222c530b69e12e9fe4701f6c07b9ed8183d3
Emiliano Ceroncd Waifu-Administratorgit config --global user.name Emiliano <hceronj1400@alumno.ipn.mx>
2021-07-24 12:53:12 -0500

    Project Finished

---
+++ JS/crud_ajax.js
@@ -0,0 +1,140 @@
+const d = document,
+  $table = d.querySelector(".crud-table"),
+  $form = d.querySelector(".crud-form"),
+  $title = d.querySelector(".crud-title"),
+  $template = d.getElementById("crud-template").content,
+  $fragment = d.createDocumentFragment();
+
+const getAll = async () => {
+  try {
+    let res = await fetch("http://localhost:3000/santos"),
+      json = await res.json();
+
+    if (!res.ok) throw { status: res.status, statusText: res.statusText };
+
+    console.log(json);
+
+    json.forEach((el) => {
+      $template.querySelector(".name").textContent = el.nombre;
+      $template.querySelector(".constellation").textContent = el.constelacion;
+      $template.querySelector(".edit").dataset.id = el.id;
+      $template.querySelector(".edit").dataset.name = el.nombre;
+      $template.querySelector(".edit").dataset.constellation = el.constelacion;
+      $template.querySelector(".delete").dataset.id = el.id;
+      $template.querySelector(".delete").dataset.name = el.nombre;
+
+      let $clone = d.importNode($template, true);
+      $fragment.appendChild($clone);
+    });
+
+    $table.querySelector("tbody").appendChild($fragment);
+  } catch (err) {
+    let message = err.statusText || "Ocurrio un error";
+    $table.insertAdjacentHTML(
+      "afterend",
+      `<p><b>Error ${err.status}: ${message}</b></p>`
+    );
+  }
+};
+
+d.addEventListener("DOMContentLoaded", getAll());
+
+d.addEventListener("submit", async (e) => {
+  if (e.target === $form) {
+    e.preventDefault();
+
+    if (!e.target.id.value) {
+      //Create - POST
+      try {
+        let options = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify({
+              nombre: e.target.nombre.value,
+              constelacion: e.target.constelacion.value,
+            }),
+          },
+          res = await fetch("http://localhost:3000/santos", options),
+          json = await res.json();
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+        location.reload();
+      } catch (error) {
+        let message = err.statusText || "Ocurrio un error";
+        $form.insertAdjacentHTML(
+          "afterend",
+          `<p><b>Error ${err.status}: ${message}</b></p>`
+        );
+      }
+    } else {
+      //Update - PUT
+      try {
+        let options = {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify({
+              nombre: e.target.nombre.value,
+              constelacion: e.target.constelacion.value,
+            }),
+          },
+          res = await fetch(
+            `http://localhost:3000/santos/${e.target.id.value}`,
+            options
+          ),
+          json = await res.json();
+
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+        location.reload();
+      } catch (err) {
+        let message = err.statusText || "Ocurrio un error";
+        $form.insertAdjacentHTML(
+          "afterend",
+          `<p><b>Error ${err.status}: ${message}</b></p>`
+        );
+      }
+    }
+  }
+});
+
+d.addEventListener("click", async (e) => {
+  if (e.target.matches(".edit")) {
+    $title.textContent = "Editar Santo"; //Cambiamos el contenido de texto del titulo
+    $form.nombre.value = e.target.dataset.name;
+    $form.constelacion.value = e.target.dataset.constellation;
+    $form.id.value = e.target.dataset.id;
+  }
+
+  if (e.target.matches(".delete")) {
+    let isDelete = confirm(
+      `¿Estás seguro de que quieres eliminar a ${e.target.dataset.name}?`
+    );
+    if (isDelete) {
+      try {
+        let options = {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json; charset=utf-8",
+            },
+          },
+          res = await fetch(
+            `http://localhost:3000/santos/${e.target.dataset.id}`,
+            options
+          ),
+          json = await res.json();
+
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+        location.reload();
+      } catch (err) {
+        let message = err.statusText || "Ocurrio un error";
+        alert(`Error: ${err.status}: ${message}`);
+      }
+    }
+  }
+});
+
+//Para este ejercicio no es necesario crear la funcion ajax que se hizo con ajax
+//Fetch ayuda mucho a simplificar las cosas
+//Para el DELETE no es necesario incluir un cuerpo debido a que solo nos intera el puro id para hacer la eliminacion
